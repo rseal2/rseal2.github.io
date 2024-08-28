0511e3c7b15be7ba7d7b45902572bf4765b8ed98
rseal2 <rseal2@huskers.unl.edu>
2024-08-27 20:56:42 -0500

    installed react, js, deleted previous website files

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react";
+import About from "./components/About";
+// import Navbar from "./components/Navbar"; 
+
+function App() {
+  return (
+    <main>
+      <About />
+    </main>
+  );
+
+}
+
+export default App;
---
+++ my-app/src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+  return (
+    <section id="about">
+        <div >
+          <h1 >
+            Hello!
+            <br />I'm Raimee Seal.
+          </h1>
+          <p >
+            I'm a computer science student interested in full-stack development.
+          </p>
+        </div>
+
+    </section>
+  );
+}
