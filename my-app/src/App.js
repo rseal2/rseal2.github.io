caf5480e96336ded7448f884def7ce5e2bf8497b
rseal2 <rseal2@huskers.unl.edu>
2024-08-28 21:43:55 -0500

    put experience text on page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from "react";
 import About from "./components/About";
 // import Navbar from "./components/Navbar"; 
+import Experience from './components/Experience';
 
 function App() {
   return (
     <main>
       <About />
+      <Experience />
     </main>
   );
 
--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,25 +1,30 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import "./About.css";
 import headshot from "../images/website-headshot.png";
 
 export default function About() {
   return (
-    <section id="about">
+    <>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Helmet>
+      <section id="about">
         <div className='textContainer'>
-          <h1 >
+          <h1>
             Hello!
           </h1>
           <h2>
             I'm Raimee Seal.
           </h2>
-          <p >
+          <p>
             I'm a computer science student interested in full-stack development.
           </p>
         </div>
-        <div >
-          <img src={headshot} alt='profilePic' className='profilePicture' />  
+        <div>
+          <img src={headshot} alt='profilePic' className='profilePicture' />
         </div>
-
-    </section>
+      </section>
+    </>
   );
 }
