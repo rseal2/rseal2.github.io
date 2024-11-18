cd2c6f388eb546e760e1ecebe5b6be686dae8bf3
rseal2 <rseal2@huskers.unl.edu>
2024-11-18 15:09:34 -0600

    added nav bar

--- portfolio-website/src/app/page.js
+++ portfolio-website/src/app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
 import Introduction from "./components/Introduction";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black container mx-auto py-4">
+    <main className="flex min-h-screen flex-col container mx-auto py-20">
+      <Navbar />
         <container className="container mx-auto px-12 py-4">
           <Introduction />
         </container>
