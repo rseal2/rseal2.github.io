a212ddee710a213d8ce0a72e9894aff659d53a6d
rseal2 <rseal2@huskers.unl.edu>
2024-11-19 21:38:29 -0600

    no more nav bar

--- portfolio-website/src/app/page.js
+++ portfolio-website/src/app/page.js
@@ -1,5 +1,4 @@
 import Introduction from "./components/Introduction";
-import Navbar from "./components/Navbar";
 import About from  "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects"; 
@@ -7,7 +6,6 @@ import Projects from "./components/Projects";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col container mx-auto py-20">
-      <Navbar />
         <container className="container mx-auto px-12 py-4">
           <Introduction />
         </container>
