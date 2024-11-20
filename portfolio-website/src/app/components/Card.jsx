221198cb5f34c6dac631bde97f05722dd1ec30fe
rseal2 <rseal2@huskers.unl.edu>
2024-11-19 21:30:38 -0600

    fixed card shape and overlay

--- portfolio-website/src/app/components/Card.jsx
+++ portfolio-website/src/app/components/Card.jsx
@@ -5,10 +5,10 @@ const Card = ({ imgUrl, title, description, onClick }) => {
     <div onClick={onClick} className="cursor-pointer">
       {" "}
       <div
-        className="h-52 md:h-72 rounded-t-xl relative group"
+        className="h-52 md:h-72 rounded-t-xl rounded-b-xl relative group"
         style={{ background: `url(${imgUrl}) center center / cover no-repeat` }}
       >
-        <div className="overlay absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500"></div>
+        <div className="overlay absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 rounded-t-xl rounded-b-xl transition-all duration-500"></div>
       </div>
       <div className="text-white rounded-b-xl bg-[#181818]py-6 px-4"> 
         <h5 className="text-xl font-semibold mb-2 mt-5">{title}</h5>
