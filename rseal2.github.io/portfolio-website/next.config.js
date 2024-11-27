a2e4f482b8797b86af494429b2a2c50bafd0b4eb
rseal2 <rseal2@huskers.unl.edu>
2024-11-26 18:22:21 -0600

    config

--- rseal2.github.io/portfolio-website/next.config.js
+++ rseal2.github.io/portfolio-website/next.config.js
@@ -1,11 +1,8 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enables static exports
-    images: {
-      unoptimized: true, // Necessary for GitHub Pages to handle images
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  basePath: "/portfolio-website",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
